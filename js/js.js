338a1e3293707c23c0be80bcfc8cc1ff547ad6e2
tymur <wkill95@gmail.com>
2016-11-13 21:42:52 +0200

    - Добавлена возможность удаления поста

--- js/js.js
+++ js/js.js
@@ -1,10 +1,18 @@
-$("#bl").blur(function () {
-	$("#disp").css("display","none");
-	$("#displ").css("display","none");
-}
-	);
-$("#bl").focus(function () {
-	$("#disp").css("display","block");
-	$("#displ").css("display","block");
-}
-	);
+$(function () {
+
+    $("#bl").blur(function () {
+        $("#disp").css("display", "none");
+        $("#displ").css("display", "none");
+    });
+
+    $("#bl").focus(function () {
+        $("#disp").css("display", "block");
+        $("#displ").css("display", "block");
+    });
+
+    // Удаление поста
+    $('[title="Delete"]').click(function(){
+        $(this).closest('li.list-group-item').detach()
+    })
+})
+
