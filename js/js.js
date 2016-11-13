e457ae6a2b1690cfefeb5ae7236f15f53bb990dc
Вика <darla.moonlight@gmail.com>
2016-11-13 20:45:09 +0200

    style + js

---
+++ js/js.js
@@ -0,0 +1,10 @@
+$("#bl").blur(function () {
+	$("#disp").css("display","none");
+	$("#displ").css("display","none");
+}
+	);
+$("#bl").focus(function () {
+	$("#disp").css("display","block");
+	$("#displ").css("display","block");
+}
+	);
